9a045182e2ca73535fb6f4e23eae82a4826079ad
duydt2 <truongduy20226@gmail.com>
2024-07-25 17:13:24 +0700

    add set mariadb_slave_capability to 4

--- tap_mysql/sync_strategies/binlog.py
+++ tap_mysql/sync_strategies/binlog.py
@@ -81,6 +81,12 @@ def verify_binlog_config(mysql_conn):
             if binlog_row_image != 'FULL':
                 raise Exception(f"Unable to replicate binlog stream because binlog_row_image is "
                                 f"not set to 'FULL': {binlog_row_image}.")
+            
+            # When replicating from Mariadb 10.6.12 using binlog coordinates, a slave capability < 4 triggers a bug in
+            # Mariadb, when it tries to replace GTID events with dummy ones. Given that this library understands GTID
+            # events, setting the capability to 4 circumvents this error.
+            # If the DB is mysql, this won't have any effect so no need to run this in a condition
+            cur.execute("SET @mariadb_slave_capability=4")
 
 
 def verify_gtid_config(mysql_conn: MySQLConnection):
